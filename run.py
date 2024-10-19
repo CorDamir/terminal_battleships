fe87cbc6714eb3b873a8d89e7489f337614584f7
Damir Ćorić <cordamir@gmail.com>
2024-10-19 02:54:13 +0000

    Set number of battleships to be 20 percent of total board fields, add random battleship allocation functionality

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
+import random
+
 class Board():
-    def __init__(self, size, battleships, player):
+    def __init__(self, size, player):
         self.size = size
-        self.battleships = battleships
+        self.battleships = int(size * size / 5)
         self.player = player
         self.current_status = []
 
@@ -27,12 +29,26 @@ class Board():
             print()
             y += 1
 
+    def randomize_battleship_locations(self):
+        ships_left_to_assign = self.battleships
+        while(ships_left_to_assign):
+            row = random.randint(0, 4)
+            column = random.randint(0, 4)
+            if self.current_status[row][column] == "*":
+                self.current_status[row][column] = "O"
+                ships_left_to_assign -= 1
 
 name = input("Your game tag:\n")
-players_board = Board(7, 5, name)
-computers_board = Board(7, 5, "Computer")
-print()
-players_board.draw_self()
-print()
-computers_board.draw_self()
-print()
+players_board = Board(7, name)
+computers_board = Board(7, "Computer")
+players_board.randomize_battleship_locations()
+computers_board.randomize_battleship_locations()
+
+def temp_print_test():
+    print()
+    players_board.draw_self()
+    print()
+    computers_board.draw_self()
+    print()
+
+temp_print_test()
