1aa9dfb24b03be439be5e6777509d4997f57cf59
Damir Ćorić <cordamir@gmail.com>
2024-10-20 02:38:24 +0000

    Edit code for better readability, minor game message adjustments

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ game_message = {
     "player": "",
     }
 
+
 class Board():
     def __init__(self, size, player):
         self.size = size
@@ -23,23 +24,23 @@ class Board():
         With argument true hides battleship locations
         """
         for i in range(self.size + 1):
-            print(f" {i} ", end = "")
-        
+            print(f" {i} ", end="")
+
         print()
-        
-        for i in range(self.size +1):
-            print("===", end = "")
+
+        for i in range(self.size + 1):
+            print("===", end="")
 
         print()
 
         y = 1
         for row in self.current_status:
-            print(f" {y}|", end = "")
+            print(f" {y}|", end="")
             for field in row:
                 if hidden and field == "O":
-                    print(f" * ", end = "")
+                    print(f" * ", end="")
                 else:
-                    print(f" {field} ", end = "")
+                    print(f" {field} ", end="")
             print()
             y += 1
 
@@ -48,31 +49,35 @@ class Board():
         Puts battleships to random locations on board
         """
         ships_left_to_assign = self.battleships
-        
-        while(ships_left_to_assign):
+
+        while (ships_left_to_assign):
             row = random.randint(0, self.size - 1)
             column = random.randint(0, self.size - 1)
-            
+
             if self.current_status[row][column] == "*":
                 self.current_status[row][column] = "O"
                 ships_left_to_assign -= 1
+
 
 def draw_game_status():
     """
     Clears screen then draws player and computer boards
     and displays score
     """
-    print("\033c", end = "")
+    print("\033c", end="")
     players_board.draw_self(False)
     print()
     computers_board.draw_self(True)
     print()
-    print(f"{players_board.name}: {players_board.battleships}    Computer: {computers_board.battleships}\n")
+
+    print(f"{players_board.name}: {players_board.battleships}    "
+          f"Computer: {computers_board.battleships}\n")
 
     if game_message["player"]:
         print(f"{game_message["player"]}")
     if game_message["computer"]:
         print(f"{game_message["computer"]}\n")
+
 
 def handle_user_guess(row, column):
     """
@@ -84,25 +89,31 @@ def handle_user_guess(row, column):
         case "O":
             computers_board.current_status[row][column] = "X"
             computers_board.battleships -= 1
-            game_message["player"] = f"You destroyed enemy battleship at ({row+1}, {column+1}) coordinates!"
+            game_message["player"] = (f"You destroyed enemy battleship at "
+                                      f"({row+1}, {column+1}) coordinates!")
             return True
 
         case "X" | "-":
-            game_message["player"] = f"You already hit ({row+1}, {column+1}) coordinates. Try again.\n"
+            game_message["player"] = (f"You already hit ({row+1}, {column+1}) "
+                                      f"coordinates. Try again.\n")
             game_message["computer"] = ""
             return False
-        
+
         case "*":
             computers_board.current_status[row][column] = "-"
-            game_message["player"] = f"No enemy battleships at ({row+1}, {column+1}) coordinates. A miss!"
+            game_message["player"] = (
+                                f"No enemy battleships at "
+                                f"({row+1}, {column+1}) coordinates. A miss!"
+                                )
             return True
+
 
 def computer_guess():
     """
     Makes a random non-repeated guess and adjusts the board
     status accordingly
     """
-    while(True):
+    while (True):
         row_guess = random.randint(0, computers_board.size - 1)
         column_guess = random.randint(0, computers_board.size - 1)
 
@@ -110,16 +121,25 @@ def computer_guess():
             case "O":
                 players_board.current_status[row_guess][column_guess] = "X"
                 players_board.battleships -= 1
-                game_message["computer"] = f"Computer destroyed your battleship at ({row_guess + 1}, {column_guess + 1}) coordinates!"
+                game_message["computer"] = (
+                                f"Computer destroyed your battleship "
+                                f"at ({row_guess + 1}, {column_guess + 1})"
+                                f" coordinates!"
+                                )
                 return
 
             case "*":
                 players_board.current_status[row_guess][column_guess] = "-"
-                game_message["computer"] = f"Computer missed at ({row_guess + 1}, {column_guess + 1}) coordinates!"
+                game_message["computer"] = (
+                            f"Computer missed at "
+                            f"({row_guess + 1}, {column_guess + 1}) "
+                            f"coordinates!"
+                            )
                 return
 
             case _:
                 pass
+
 
 def get_user_guess():
     """
@@ -129,23 +149,38 @@ def get_user_guess():
     row_guess = input("Row:\n")
     column_guess = input("Column:\n")
 
-    try: #convert user input to integers
+    try:
+        # convert user input to integers
         row_guess = int(row_guess) - 1
         column_guess = int(column_guess) - 1
 
-    except: #display try again message to player if not integers
-        game_message["player"] = "Row and Column must be single numbers. Try again.\n"
+    except Exception:
+        # display try again message to player if not integers
+        game_message["player"] = (
+            "Row and Column must be single numbers. Try again.\n"
+            )
         game_message["computer"] = ""
 
-    else:  #otherwise check if input is within board limits
-        if column_guess >= 0 and column_guess < players_board.size and row_guess >= 0 and row_guess < players_board.size:
-            #within limits: execute user input handling function
-            return handle_user_guess(row_guess, column_guess) #return handling functions result
-        else: #outside limits: display a try again message to user
-            game_message["player"] = "That hit would be outside the battlefield. Try again.\n"
-            game_message["computer"] = ""
+    else:
+        # otherwise check if input is within board limits
+        if (
+          column_guess >= 0 and
+          column_guess < players_board.size and
+          row_guess >= 0 and
+          row_guess < players_board.size
+          ):
+            # within limits: execute user input handling function
+            return handle_user_guess(row_guess, column_guess)
 
-    return False  
+        else:
+            # outside limits: display a try again message to user
+            game_message["player"] = (
+                "That hit would be outside the battlefield. Try again.\n"
+                )
+            game_message["computer"] = ""
+    # if user input not validated return false to loop user input
+    return False
+
 
 def end_game(player_won):
     """
@@ -153,25 +188,27 @@ def end_game(player_won):
     and declares a winner. Use True argument for player win and
     false for computer win
     """
-    print("\033c", end = "")
+    print("\033c", end="")
     if player_won:
         computers_board.draw_self(False)
-        print (f"\nCongratulations {players_board.name}, you WON!\n")
+        print(f"\nCongratulations {players_board.name}, you WON!\n")
     else:
         players_board.draw_self(False)
-        print (f"\nComputer wins this one.\n")
+        print(f"\nComputer wins this one.\n")
 
     input("Press enter to reset...")
     main()
 
+
 def display_welcome_screen():
     """
-    Displays simple instructons before 
+    Displays simple instructons before
     the game start
     """
-    print("\033c", end = "")
+    print("\033c", end="")
+    
     print(
-        "\tWelcome to Battleships game!\n\n" 
+        "\tWelcome to Battleships game!\n\n"
         "After you enter your game tag, your battle board will be\n"
         "displayed first and computers second with following logic:\n\n"
         "'*' - unchecked field\n"
@@ -183,29 +220,37 @@ def display_welcome_screen():
         "Enter numbers of row and column for the field you wish to\n"
         "check and the result will be displayed both on the battlefield\n"
         "and in text below. Destroy computers battleships before yours\n"
-        "are destroyed and win!\n"
+        "are destroyed and win!\n\n"
+        "Ctrl+c if you wish to stop game.\n"
         )
+    
     if game_message["player"]:
-        print (game_message["player"])
+        print(game_message["player"])
+
 
 def game_loop():
-    """ Game loop - end_game() function exits"""
-    while(True):
+    """Game loop"""
+    while (True):
         draw_game_status()
 
-        if get_user_guess(): #if user guess is validated and not repeated continue with game
-            if not computers_board.battleships: #if player won the game
+        if get_user_guess():
+            # if user guess is validated and not repeated continue with game
+            if not computers_board.battleships:
+                # if player won the game
                 end_game(True)
 
             computer_guess()
 
-            if not players_board.battleships: #if computer won the game
+            if not players_board.battleships:
+                # if computer won the game
                 end_game(False)
 
+
 def main():
-    while(True):
+    while (True):
         display_welcome_screen()
         name = input("Your game tag (3-12):\n")
+        
         if (len(name) > 12 or len(name) < 3):
             game_message["player"] = "Game tag should be 3-12 characters long"
         else:
@@ -219,5 +264,6 @@ def main():
     computers_board = Board(5, "Computer")
 
     game_loop()
+
 
 main()
